e4c1b1b696214ee81e6beb41c9fa9ea5cb6726b9
diveyum <65844451+diveyum@users.noreply.github.com>
2026-04-28 11:02:29 +0530

    Create page.tsx

---
+++ src/app/tabicl/page.tsx
@@ -0,0 +1,5 @@
+import IframeWrapper from '@/components/IframeWrapper';
+
+export default function TabICLPage() {
+  return <IframeWrapper src="/portfolio/tabicl-guide/index.html" title="The Illustrated Tabular Foundation Model" />;
+}
